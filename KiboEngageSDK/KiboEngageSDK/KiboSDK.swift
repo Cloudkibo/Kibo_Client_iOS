d3a2e793a4518f321b1e88ad69b9fb1e76b46b95
sumairasaeed <sumaira.syedsaeed@gmail.com>
2016-09-16 01:58:42 +0500

    azure push notifications

--- KiboEngageSDK/KiboEngageSDK/KiboSDK.swift
+++ KiboEngageSDK/KiboEngageSDK/KiboSDK.swift
@@ -13,6 +13,8 @@ import UIKit
 
 public class KiboSDK{
     
+    
+    var aaa:SBNotificationHub!
   //  var kiboAppID=""
    // var kiboAppSecret=""
    // var kiboClientID=""
@@ -64,7 +66,145 @@ public class KiboSDK{
         
         ////sqliteDB=DatabaseHandler(dbName:"kiboEngageDB.sqlite3")
         
+        let notificationTypes: UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound]
+        
+        //let notificationTypes: UIUserNotificationType = [UIUserNotificationType.None]
+        
+        
+        let pushNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
+        
+        
+        
+        /////-------will be commented----
+        //application.registerUserNotificationSettings(pushNotificationSettings)
+        //application.registerForRemoteNotifications()
+        
+        
+        //^^^^^^^^^^^^^^^^^^^
+        
+        //  print("username is \(username!)")
+        //if(username != nil && username != "")
+        //{
+            UIApplication.sharedApplication().registerUserNotificationSettings(pushNotificationSettings)
+        //}
+        
+        
+
+        
     }
+    
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        print("didRegisterUserNotificationSettings")
+        //if(!UIApplication.sharedApplication().isRegisteredForRemoteNotifications())
+        // {
+       // if(username != nil && username != "")
+       // {
+            print("didRegisterUserNotificationSettings... inside...")
+            
+            UIApplication.sharedApplication().registerForRemoteNotifications()
+        //}
+        
+        // }
+        
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        print("trying to register device token")
+       // if(username != nil && username != ""){
+            print("inside didRegisterForRemoteNotificationsWithDeviceToken ")
+          ////////////  var hub=SBNotificationHub(connectionString: Constants.connectionstring, notificationHubPath: Constants.hubname) //from constants file
+            var tagarray=[String]()
+            tagarray.append("+923201211991")
+          //  print(username!.substringFromIndex(username!.startIndex.successor()))
+            // var tagname=NSSet(object: username!.substringFromIndex(username!.startIndex))
+            var tagname=NSSet(array: tagarray)
+            // hub.registerNativeWithDeviceToken(deviceToken, tags: tagname as Set<NSObject>) { (error) in
+            hub.registerNativeWithDeviceToken(deviceToken, tags: tagname as Set<NSObject>) { (error) in
+                //hub.registerNativeWithDeviceToken(deviceToken, tags: nil) { (error) in
+                
+                if(error != nil)
+                {
+                    print("Registering for notifications \(error)")
+                }
+                else
+                {
+                    print("Successfully registered for notifications")
+                    
+                }
+                
+           // }
+        }
+    }
+    
+    
+    /*
+     received while the app is active:
+     
+     Copy
+     - (void)application:(UIApplication *)application didReceiveRemoteNotification: (NSDictionary *)userInfo {
+     NSLog(@"%@", userInfo);
+     [self MessageBox:@"Notification" message:[[userInfo objectForKey:@"aps"] valueForKey:@"alert"]];
+     }
+     */
+    
+    
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        // NSLog("received remote notification \(userInfo)")
+        /*if(socketObj != nil)
+        {
+            socketObj.socket.emit("logClient","\(username) didReceiveRemoteNotification: \(userInfo.description)")
+        }*/
+        print("remote notification received is \(userInfo)")
+        /*var notificationJSON=JSON(userInfo)
+         print("json converted is \(notificationJSON)")
+         print("json received is is \(notificationJSON["aps"])")
+         */
+        completionHandler(UIBackgroundFetchResult.NewData)
+        NSNotificationCenter.defaultCenter().postNotificationName("ReceivedNotification", object:userInfo)
+        /*
+         json converted is {
+         "aps" : {
+         "data" : {
+         "msg" : "Hello +923201211991! You joined the room."
+         }
+         }
+         }
+         */
+        
+        
+        /*
+         var payload = {
+         +          type : im.stanza.type,
+         +          senderId : im.stanza.from,
+         +          uniqueId : im.stanza.uniqueid
+         +        };
+         +
+         +        sendPushNotification(im.stanza.to, payload);
+         +      }
+         */
+        // print("json received is is \(notificationJSON["aps"])")
+    }
+    
+    
+    /*
+     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+     let token=JSON(deviceToken)
+     print("device tokennnnnnn...", terminator: "")
+     socketObj.socket.emit("logClient","deviceToken: \(token)")
+     print(token.debugDescription)
+     
+     print("registered for notification", terminator: "")
+     }
+     */
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        
+        print("registered for notification error", terminator: "")
+        NSLog("Error in registration. Error: \(error)")
+    }
+    
     
     public func doSomething()
     {
