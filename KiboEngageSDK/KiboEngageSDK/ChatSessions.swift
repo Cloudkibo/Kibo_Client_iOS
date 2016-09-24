a3a5756916a9388d93cef973481e3330923e2a30
sumairasaeed <sumaira.syedsaeed@gmail.com>
2016-09-25 02:43:00 +0500

    work on initialising chat sessions

--- KiboEngageSDK/KiboEngageSDK/ChatSessions.swift
+++ KiboEngageSDK/KiboEngageSDK/ChatSessions.swift
@@ -22,6 +22,18 @@ public class ChatSessions
     {
         var GroupsObjectList:[[String:AnyObject]]
         GroupsObjectList = DatabaseObjectInitialiser.getDB().getGroupsObjectList()
+        var customeridDataList=[String:AnyObject]()
+        customeridDataList["customerID"]=DatabaseObjectInitialiser.getInstance().customerid
+        for keyname in DatabaseObjectInitialiser.getInstance().optionalDataList.keys {
+            print("Key: \(keyname) value: \(DatabaseObjectInitialiser.getInstance().optionalDataList[keyname]!)")
+            customeridDataList["\(keyname)"]=DatabaseObjectInitialiser.getInstance().optionalDataList[keyname]!
+        }
+        customeridDataList["isMobileClient"]=true
+        
+        
+        
+
+        
         
         for(var i=0;i<GroupsObjectList.count;i++)
         {
@@ -40,19 +52,16 @@ public class ChatSessions
          
            /// DatabaseObjectInitialiser.getInstance().optionalDataList.ke
          //GroupsObjectList[indexPath.row]["deptname"] as! String
-            var customeridDataList=[String:AnyObject]()
-            customeridDataList["customerID"]=DatabaseObjectInitialiser.getInstance().customerid
-            for keyname in DatabaseObjectInitialiser.getInstance().optionalDataList.keys {
-                print("Key: \(keyname) value: \(DatabaseObjectInitialiser.getInstance().optionalDataList[keyname]!)")
-            customeridDataList["\(keyname)"]=DatabaseObjectInitialiser.getInstance().optionalDataList[keyname]!
-            }
-         
-         
+            
          // var sessiondata=
             
             
             for(var j=0;j<channelsList.count;j++)
             {
+                
+                var randomRequestID=(DatabaseObjectInitialiser.randomStringWithLength(5) as String)+"\(GroupsObjectList[i]["_id"] as! String)"+"\(channelsList[j]["_id"] as! String)"
+                
+                print("request id for group \(i) and channel \(j) is \(randomRequestID) ")
          var agIds = [String]()
          var chArray = [String]()
          chArray.append(channelsList[j]["_id"] as! String)
@@ -62,30 +71,63 @@ public class ChatSessions
         // var customeridjson = JSON(["customerID" : DatabaseObjectInitialiser.getInstance().customerid,"email" : "aaaaa@cloudkibo.com","country" : "Pakistan","phone" :   "12323424","companyid" : channelsList[j]["companyid"] as! String,"isMobileClient":false])
                 
                 print("channel iteration is for \(channelsList[j]["msg_channel_name"] as! String)")
+                var socketDataList=[String:AnyObject]()
+                
+                socketDataList["customerid"]=customeridjson.object
+                socketDataList["departmentid"]=GroupsObjectList[i]["_id"] as! String //groupIDdepartment id
+                socketDataList["platform"]="mobile"  // "web" or “mobile”
+                socketDataList["agent_ids"]=agIds  //initially empty
+                socketDataList["group"]=GroupsObjectList[i]["deptname"] as! String //groupname
+                socketDataList["messagechannel"]=chArray // array of channel ID’s
+                socketDataList["channelname"]=channelsList[j]["msg_channel_name"] as! String //name of currnet channel choosen
+                //fullurl :  fullurl, //optional
+                // currentPage : pathname, //optional
+                if(customeridDataList["phone"] != nil)
+                 {
+                 socketDataList["phone"]=customeridDataList["phone"] as! String
+                 
+                 }
+                socketDataList["requesttime"]=NSDate().description
+                socketDataList["status"]="new" //initial value “new”
+                socketDataList["device"]="iOS" //android or iOS
+                ////socketDataList["device_version"]="9.3"
+                ///ipAddress:'192.168.1.2', //optional
+                socketDataList["Is_rescheduled"]=false  //initially false
+                socketDataList["initiator"]="visitor"
+                socketDataList["companyid"]=DatabaseObjectInitialiser.getInstance().clientid //get from host app 'clientID'
+                socketDataList["room"]=DatabaseObjectInitialiser.getInstance().clientid
+                socketDataList["request_id"]=randomRequestID //generate urself unique id and save
+                //webrtc_browser :'true',//optional
+                socketDataList["msg"]="User joined session"
+               
+                print("socket data is \(socketDataList.debugDescription)")
+                //////////====DatabaseObjectInitialiser.getInstance().socketObj.socket.emit("join meeting",socketDataList)
                 
                 
-                
-                
-                
-               /*  var customerid = {‘customerID’ : //mandatory,
+                /*
+                 var customerid = {‘customerID’ : //mandatory,
                  'name' : name.value //optional : remove key if not available,
                  'email' : email.value //optional,
                  'country' : country.value //optional,
                  'phone' :   phone.value //optional,
                  'companyid' : companyid,
-                 'isMobileClient':"false"}
+                 'isMobileClient':"false"}*/
                  
-                 DatabaseObjectInitialiser.getInstance().socketObj.socket.emit("join meeting",
-                 customerid : customerid,
-                 departmentid : String //groupIDdepartment id
-                 platform :String  // "web" or “mobile”
+                /* DatabaseObjectInitialiser.getInstance().socketObj.socket.emit("join meeting",
+                 customerid : customeridjson.object,
+                 departmentid : GroupsObjectList[i]["_id"] as String, //groupIDdepartment id
+                 platform :"mobile",  // "web" or “mobile”
                  agent_ids : agIds,  //initially empty
-                 group:this.refs.grouplist.options[this.refs.grouplist.selectedIndex].text, //groupname
+                 group:GroupsObjectList[i]["deptname"] as String, //groupname
                  messagechannel : chArray, // array of channel ID’s
-                 channelname: String, //name of currnet channel choosen
-                 fullurl :  fullurl, //optional
-                 currentPage : pathname, //optional
-                 phone :  phone.value,
+                 channelname : channelsList[j]["msg_channel_name"] as String, //name of currnet channel choosen
+                 //fullurl :  fullurl, //optional
+                // currentPage : pathname, //optional
+                    /*if(customeridDataList["phone"] != nil)
+                    {
+                    phone : customeridDataList["phone"] as String,
+
+                    }*/
                  requesttime:Date.now(),
                  status : "new", //initial value “new”
                  device : “iOS”, //android or iOS
@@ -98,8 +140,8 @@ public class ChatSessions
                  request_id : unique_id, //generate urself unique id and save
                  webrtc_browser :'true',//optional
                  msg : 'User joined session',
-                 };
-*/
+                 };*/
+
  
                 
                 //=====
